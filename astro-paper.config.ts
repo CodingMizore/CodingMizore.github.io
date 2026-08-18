933f2fa8055efab4f2505cbb17cabc3103af1df9
labbingTomorin319 <3136658002@qq.com>
2026-08-18 16:36:19 +0800

    fix about

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -3,7 +3,7 @@ import { defineAstroPaperConfig } from "./src/types/config";
 export default defineAstroPaperConfig({
   site: {
     url: "https://CodingMizore.github.io/",
-    title: "AstroPaper",
+    title: "CodingMizore Blog",
     description: "A minimal, responsive and SEO-friendly Astro blog theme.",
     author: "Sat Naing",
     profile: "https://satna.ing",
